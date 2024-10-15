621f3d14e880bfb2b382276dbe7ba4054508670d
cheng zhen <chengchongzhen6@gmail.com>
2024-10-15 17:34:08 +0800

    refactor: 重构类型定义并将通用接口移至 typing.d.ts

---
+++ src/typing/typing.d.ts
@@ -0,0 +1,39 @@
+interface ResponseData<T> {
+  success: boolean
+  data: T | null
+  error: string | null
+}
+
+interface DeviceInfo {
+  batteryInfo: {
+    level: number
+    acPowered: boolean
+    usbPowered: boolean
+    wirelessPowered: boolean
+    maxChargingCurrent: number
+    maxChargingVoltage: number
+    chargeCounter: number
+    status: number
+    health: number
+    present: boolean
+    scale: number
+    voltage: number
+    temperature: number
+    technology: string
+  }
+  serialNumber: string
+  screenResolution: string
+  screenSize: string
+  model: string
+  androidVersion: string
+  manufacturer: string
+  totalStorage: string
+  usedStorage: string
+  availableStorage: string
+  totalMemoryGB: string
+  availableMemoryGB: string
+  usedMemoryGB: string
+  marketingName: string
+  isWifiEnabled: boolean
+  currentWifi: string
+}
